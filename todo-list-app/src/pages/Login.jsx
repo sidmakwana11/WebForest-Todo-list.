0e15373e9b7e8fd84265a044ca5ba0e2f1a9c90e
Siddharth Makwana <112619536+sidmakwana11@users.noreply.github.com>
2025-04-04 16:17:38 +0530

    Update Login.jsx

--- todo-list-app/src/pages/Login.jsx
+++ todo-list-app/src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     console.log("Sending Login Request:", requestBody); 
   
     try {
-      const response = await fetch("http://localhost:4001/login", {
+      const response = await fetch("https://todo-list-backend-9jhe.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
